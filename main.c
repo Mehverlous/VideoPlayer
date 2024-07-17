25313d6cad9e21d52320ce02e157bb4fae1d9255
Mehverlous <35834357+Mehverlous@users.noreply.github.com>
2024-07-17 14:48:03 -0400

    Displaying a simple image

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ static void print_hello(GtkWidget *widget, gpointer data){
 
 static void activate(GtkApplication *app, gpointer user_data){
   GtkWidget *window;
-  GtkWidget *button;
+  GtkWidget *image = gtk_image_new_from_file ("Test_Images/TestImage2.jpeg");
+
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Hello");
   gtk_window_set_default_size(GTK_WINDOW(window), 1080, 720);
 
-  button = gtk_button_new_with_label("Hello World");
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  gtk_window_set_child(GTK_WINDOW(window), button);
+
+  gtk_window_set_child(GTK_WINDOW(window), image);
 
   gtk_window_present(GTK_WINDOW(window));
 }
@@ -23,7 +23,7 @@ int main(int argc, char **argv){
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new("main.gtk.video_player", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
