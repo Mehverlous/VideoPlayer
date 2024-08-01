b71d3e33fa6f65ae77e61014be6e2ff35c636c0d
Mehverlous <35834357+Mehverlous@users.noreply.github.com>
2024-08-01 15:20:51 -0400

    Drawing Area init

--- TestCarousel.c
+++ TestCarousel.c
@@ -2,97 +2,49 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
-int counter = 1;
-gint timer;
+static void
+draw_function(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer data){
+  GdkRGBA color;
 
-static void start_timer();
-static void draw_function (GtkDrawingArea *, cairo_t *, int, int, gpointer);
-static void increment_counter(GtkWidget *);
+  cairo_arc(cr, width / 2.0, height / 2.0, MIN (width, height) / 2.0, 0, 2 * G_PI);
 
-static void start_timer(GtkWidget *darea){
-  timer = g_timeout_add(650, (GSourceFunc) increment_counter, darea);
-}
+  gtk_widget_get_color(GTK_WIDGET(area), &color);
+  gdk_cairo_set_source_rgba(cr, &color);
 
-static void stop_timer(){
-  g_source_remove(timer);
-}
-
-static void increment_counter(GtkWidget *darea){
-  if(counter > 4){
-    counter = 1;
-  }else{
-    counter++;
-  }
-
-  gtk_widget_queue_draw(darea);
-}
-
-static void draw_function(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer data){
-  cairo_surface_t *image1 = cairo_image_surface_create_from_png("Test_Images/TestImage1.png");
-  cairo_surface_t *image2 = cairo_image_surface_create_from_png("Test_Images/TestImage2.png");
-  cairo_surface_t *image3 = cairo_image_surface_create_from_png("Test_Images/TestImage3.png");
-  cairo_surface_t *image4 = cairo_image_surface_create_from_png("Test_Images/TestImage4.png");
-  cairo_surface_t *image5 = cairo_image_surface_create_from_png("Test_Images/TestImage5.png");
-
-  cairo_surface_t *painted_image = cairo_image_surface_create_from_png("Test_Images/TestImage1.png");
-
-  if(counter == 1){
-    painted_image = image1;
-  }else if (counter == 2){
-    painted_image = image2;
-  }else if (counter == 3){
-    painted_image = image3;
-  }else if (counter == 4){
-    painted_image = image4;
-  }else if (counter == 5){
-    painted_image = image5;
-  }
-
-  cairo_set_source_surface(cr, painted_image, 75, 0);
-  cairo_paint(cr);
+  cairo_fill(cr);
 }
 
 static void activate(GtkApplication *app, gpointer user_data){
-  GtkWidget *window, *start_timer_button,*stop_timer_button, *frame, *box, *dareaFrame, *darea;
+    GtkWidget *window, *vbox, *darea, *da_container;
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Slideshow");
-  gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    //initializing the window
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Testing Carousel");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1080, 720);
 
-  frame = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-  gtk_widget_set_halign(frame, GTK_ALIGN_CENTER);
-  gtk_widget_set_valign(frame, GTK_ALIGN_CENTER);
+    //creating a container to store the main widgets (drawing area and buttons)
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_widget_set_margin_start(vbox, 10);
+    gtk_widget_set_margin_end(vbox, 10);
+    gtk_widget_set_margin_top(vbox, 10);
+    gtk_widget_set_margin_bottom(vbox, 10);
+    gtk_window_set_child(GTK_WINDOW(window), vbox);
 
-  box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-  gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
-  gtk_widget_set_valign(box, GTK_ALIGN_START);
+    //creating frame to outline the position of the drawing area
+    da_container = gtk_frame_new(NULL);
+    gtk_widget_set_vexpand(da_container, TRUE);
+    gtk_widget_set_hexpand(da_container, TRUE);
+    gtk_box_append(GTK_BOX(vbox), da_container);
 
-  gtk_window_set_child(GTK_WINDOW(window), frame);
+    //initializing the drawing area
+    darea = gtk_drawing_area_new();
+    gtk_frame_set_child(GTK_FRAME(da_container), darea);
+    gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(darea), 1050);
+    gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(darea), 700);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(darea), draw_function, NULL, NULL);
 
-  dareaFrame = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-  gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
-  gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
-
-  darea = gtk_drawing_area_new();
-  gtk_drawing_area_set_content_width (GTK_DRAWING_AREA (darea), 750);
-  gtk_drawing_area_set_content_height (GTK_DRAWING_AREA (darea), 400);
-  gtk_drawing_area_set_draw_func (GTK_DRAWING_AREA (darea), draw_function, NULL, NULL);
-
-  gtk_box_append(GTK_BOX(dareaFrame), darea);
-
-  start_timer_button = gtk_button_new_with_label("Start Timer");
-  gtk_box_append(GTK_BOX(box), start_timer_button);
-  g_signal_connect_swapped(start_timer_button, "clicked", G_CALLBACK(start_timer), darea);
-
-  stop_timer_button = gtk_button_new_with_label("Stop Timer");
-  gtk_box_append(GTK_BOX(box), stop_timer_button);
-  g_signal_connect_swapped(stop_timer_button, "clicked", G_CALLBACK(stop_timer), NULL);
-
-  gtk_box_append(GTK_BOX(frame), dareaFrame);
-  gtk_box_append(GTK_BOX(frame), box);
-
-  gtk_window_present(GTK_WINDOW(window));
-
+    
+    gtk_window_present(GTK_WINDOW(window));
 }
 
 int main (int argc,char **argv){
--- main.c
+++ main.c
@@ -6,58 +6,85 @@ struct Frame{
     GtkWidget *image;
 };
 
-static void carousel(GtkWidget *window, GtkWidget *screen){
-    GtkWidget *image1 = gtk_picture_new_for_filename("Test_Images/TestImage1.png");
-    GtkWidget *image2 = gtk_picture_new_for_filename("Test_Images/TestImage2.png");
-    GtkWidget *image3 = gtk_picture_new_for_filename("Test_Images/TestImage3.png");
-    GtkWidget *image4 = gtk_picture_new_for_filename("Test_Images/TestImage4.png");
-    GtkWidget *image5 = gtk_picture_new_for_filename("Test_Images/TestImage5.png");
+// static void carousel(GtkWidget *window, GtkWidget *screen){
+//     GtkWidget *image1 = gtk_picture_new_for_filename("Test_Images/TestImage1.png");
+//     GtkWidget *image2 = gtk_picture_new_for_filename("Test_Images/TestImage2.png");
+//     GtkWidget *image3 = gtk_picture_new_for_filename("Test_Images/TestImage3.png");
+//     GtkWidget *image4 = gtk_picture_new_for_filename("Test_Images/TestImage4.png");
+//     GtkWidget *image5 = gtk_picture_new_for_filename("Test_Images/TestImage5.png");
 
-    struct Frame frames[5];
-    frames[0].image = image1;
-    frames[1].image = image2;
-    frames[2].image = image3;
-    frames[3].image = image4;
-    frames[4].image = image5;
+//     struct Frame frames[5];
+//     frames[0].image = image1;
+//     frames[1].image = image2;
+//     frames[2].image = image3;
+//     frames[3].image = image4;
+//     frames[4].image = image5;
 
-    GtkWidget *currentImage = frames[0].image;
+//     GtkWidget *currentImage = frames[0].image;
 
-    gtk_box_append(GTK_BOX(screen), currentImage);
+//     gtk_box_append(GTK_BOX(screen), currentImage);
 
-    gtk_window_present(GTK_WINDOW(window));
+//     gtk_window_present(GTK_WINDOW(window));
 
-    int i = 0;
+//     int i = 0;
 
-    // while(1){
-    //     if(i == 4){
-    //         i = 0;
-    //     }
+//     // while(1){
+//     //     if(i == 4){
+//     //         i = 0;
+//     //     }
 
-    //     sleep(1);
+//     //     sleep(1);
 
-    //     currentImage = frames[i].image;
-    //     printf("Changed to image: %d\n", i+1);
+//     //     currentImage = frames[i].image;
+//     //     printf("Changed to image: %d\n", i+1);
 
-    //     i++;
-    // }
+//     //     i++;
+//     // }
+// }
+
+static void
+draw_function(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer data){
+  GdkRGBA color;
+
+  cairo_arc(cr, width / 2.0, height / 2.0, MIN (width, height) / 2.0, 0, 2 * G_PI);
+
+  gtk_widget_get_color(GTK_WIDGET(area), &color);
+  gdk_cairo_set_source_rgba(cr, &color);
+
+  cairo_fill(cr);
 }
 
 static void activate(GtkApplication *app, gpointer user_data){
-    GtkWidget *window, *screen;
+    GtkWidget *window, *vbox, *darea, *da_container;
 
+    //initializing the window
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Main Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1080, 720);
 
-    screen = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_widget_set_halign(screen, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(screen, GTK_ALIGN_CENTER);
+    //creating a container to store the main widgets (drawing area and buttons)
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_widget_set_margin_start(vbox, 10);
+    gtk_widget_set_margin_end(vbox, 10);
+    gtk_widget_set_margin_top(vbox, 10);
+    gtk_widget_set_margin_bottom(vbox, 10);
+    gtk_window_set_child(GTK_WINDOW(window), vbox);
 
-    gtk_window_set_child(GTK_WINDOW(window), screen);
+    //creating frame to outline the position of the drawing area
+    da_container = gtk_frame_new(NULL);
+    gtk_widget_set_vexpand(da_container, TRUE);
+    gtk_widget_set_hexpand(da_container, TRUE);
+    gtk_box_append(GTK_BOX(vbox), da_container);
 
+    //initializing the drawing area
+    darea = gtk_drawing_area_new();
+    gtk_frame_set_child(GTK_FRAME(da_container), darea);
+    gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(darea), 1050);
+    gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(darea), 700);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(darea), draw_function, NULL, NULL);
 
-    carousel(window, screen);
-
+    
+    gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char **argv){
