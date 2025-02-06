34840b034f9b3b5f9d9d7b6f2e51b6c7aed928d0
Mehverlous <35834357+Mehverlous@users.noreply.github.com>
2025-02-05 21:50:21 -0500

    Bug fixes
    Readded the Empty.png to the Documents folder to prevent an empty GTK drawing area error upon activation

--- src/main.c
+++ src/main.c
@@ -367,7 +367,7 @@ static void activate(GtkApplication *app, gpointer user_data){
 
 	//initializing the window
 	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "Testing Carousel");
+	gtk_window_set_title(GTK_WINDOW(window), "Video Player");
 	gtk_window_set_default_size(GTK_WINDOW(window), 1280, 800);
 
 	//creating a container to store the main widgets (drawing area and buttons)
